414a2023cf38dfb0eabc2a34cf1996c9a03e02a2
Matheus Augusto <auguzsto@gmail.com>
2023-09-01 16:22:56 -0300

    fix: page 2fa

--- app/src/views/2FA.php
+++ app/src/views/2FA.php
@@ -22,10 +22,17 @@ use App\services\Database;
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Telecall - 2FA</title>
+    <link rel="stylesheet" href="/app/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/app/assets/css/style.css">
+    <title>Telecall</title>
 </head>
 <body>
+<nav class="navbar navbar-extand-lg light bg-light shadow p-3 mb-5 bg-white rounded">
+    <a href="/"><img src="app/assets/images/navbar.png" class="navbar brand img-fluid"></a>
+</nav>
+    <div class="container">
     <h1>
         <?php 
             echo $asks[$rand];
@@ -53,7 +60,7 @@ use App\services\Database;
             if($responses[$i] == $_POST['response']) {
                 $_SESSION['2fa'] = null;
                 Session::create($user);
-                
+
             } else {
                 header("Location: /2FA");
             }
--- app/src/views/login.php
+++ app/src/views/login.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="app/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="app/assets/css/style.css">
+    <link rel="stylesheet" href="/app/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/app/assets/css/style.css">
     <title>Telecall</title>
 </head>
 <body>
@@ -54,10 +54,9 @@
     </div>
 </body>
 </html>
-<script src="app/assets/js/jquery.slim.min.js"></script>
-<script src="app/assets/js/popper.min.js"></script>
-<script src="app/assets/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="app/assets/js/auth.js"></script>
+<script src="/app/assets/js/jquery.slim.min.js"></script>
+<script src="/app/assets/js/popper.min.js"></script>
+<script src="/app/assets/js/bootstrap.min.js"></script>
 
 <?php 
 
