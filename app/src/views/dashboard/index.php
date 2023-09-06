ac499d94e7e787348f6ec118b44171966ced91c7
Matheus Augusto <auguzsto@gmail.com>
2023-09-06 13:01:35 -0300

    feat: dashboard

--- app/src/views/dashboard/index.php
+++ app/src/views/dashboard/index.php
@@ -9,9 +9,11 @@ use App\services\Session;
 
 <?php include __DIR__ . "/modules/header.php"; ?>
 
-<div>
-    <h1>
-        Olá, <?php echo $user->getFirstName(); ?>
-    </h1>
-</div>
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Dashboard</h1>
+      </div>
+</main>
+
+<?php include __DIR__ . "/modules/footer.php"; ?>
 
---
+++ app/src/views/dashboard/profile.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\services\Session;
+    
+    Session::check();
+    $user = $_SESSION['session'];
+
+?>
+
+<?php include __DIR__ . "/modules/header.php"; ?>
+
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Perfil</h1>  
+    </div>
+    <form method="POST">
+        <h5>CPF</h5>
+        <input class="form-control" type="text" value="<?php echo $user->getCpf(); ?>" disabled>
+        <h5>Nome</h5>
+        <input class="form-control" type="text" value="<?php echo $user->getFirstName(); ?>" disabled>
+        <h5>Último nome</h5>
+        <input class="form-control" type="text" value="<?php echo $user->getLastName(); ?>" disabled>
+        <h5>Senha</h5>
+        <input class="form-control" type="password" value="<?php echo $user->getPassword(); ?>" disabled>
+        <h5>E-mail</h5>
+        <input class="form-control" type="text" value="<?php echo $user->getEmail(); ?>">
+        <h5>CEP</h5>
+        <input class="form-control" type="text" value="<?php echo $user->getCep(); ?>">
+        <h5>Celular</h5>
+        <input class="form-control" type="text" value="<?php echo $user->getPhone(); ?>">
+        <h5>Endereço completo</h5>
+        <input class="form-control" type="text" value="<?php echo $user->getAddress(); ?>">
+    </form>
+</main>
+<?php
+
+    include __DIR__ . "/modules/footer.php";
