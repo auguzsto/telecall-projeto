951cef3938a46882bda1206b054801d6115401dc
Anderson <119246170+devgaucho@users.noreply.github.com>
2023-09-09 09:30:24 -0300

    Update Router.php
    fix

--- app/src/services/Router.php
+++ app/src/services/Router.php
@@ -6,7 +6,9 @@ namespace App\services;
         public static function get(string $path, string $view) {
             $request = $_SERVER['REQUEST_URI'];
             $requestParts = explode('/',explode('?',$request)[0]);
-            switch($requestParts[0]) {
+            //altere a chave do array aqui caso precise
+            // (ex: URI = "/index.php/dir" -> $requestParts[0] = index.php -> $requestParts[1] = "dir"
+            switch($requestParts[1]) { 
                 case $path:
                     include $view;
                     break;
