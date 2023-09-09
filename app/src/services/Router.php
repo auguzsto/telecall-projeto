2cd64bd7661f895c6a2802143d04274429dcf1e1
Anderson <119246170+devgaucho@users.noreply.github.com>
2023-09-09 09:21:08 -0300

    Update Router.php
    quebra a URI em partes

--- app/src/services/Router.php
+++ app/src/services/Router.php
@@ -5,8 +5,8 @@ namespace App\services;
     class Router {
         public static function get(string $path, string $view) {
             $request = $_SERVER['REQUEST_URI'];
-
-            switch($request) {
+            $requestParts = explode('/',explode('?',$request)[0]);
+            switch($requestParts[0]) {
                 case $path:
                     include $view;
                     break;
