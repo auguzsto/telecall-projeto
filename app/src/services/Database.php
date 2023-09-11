26f7b2e36712bd6b88c3d6f88ca2a1c184482cf7
Matheus Augusto <auguzsto@gmail.com>
2023-09-11 13:17:51 -0300

    feat: set UPDATED_AT when used Database->update();

--- app/src/services/Database.php
+++ app/src/services/Database.php
@@ -63,7 +63,7 @@ use App\config\Config;
             $pdo = $this->con();
             $set = implode("=?, ", array_keys($columns));
             $pdo->prepare("UPDATE $table SET $set = ? WHERE $where")->execute(array_values($columns));
-            $pdo->prepare("UPDATE $table SET updated_at = ? WHERE $where")->execute([date("Y-m-d HH:mm:ss")]);
+            $pdo->prepare("UPDATE $table SET updated_at = ? WHERE $where")->execute([date('Y-m-d H:i:s')]);
         }
 
         public function select(string $columns, string $table): array {
