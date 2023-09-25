8dc31a97d2391062d7343239d7c97c41b3644a0d
Matheus Augusto <auguzsto@gmail.com>
2023-09-25 12:53:27 -0300

    feat: new version db to acl in modules by user

---
+++ app/src/db/db_25092023.php
@@ -0,0 +1,98 @@
+CREATE DATABASE IF NOT EXISTS grp_16_bangu_noite;
+
+CREATE TABLE access_control(
+    id BIGINT AUTO_INCREMENT,
+    description varchar(255) NOT NULL,
+    permission_create ENUM("Y", "N") NOT NULL,
+    permission_execute ENUM("Y", "N") NOT NULL,
+    permission_read ENUM("Y", "N") NOT NULL,
+    permission_update ENUM("Y", "N") NOT NULL,
+    permission_delete ENUM("Y", "N") NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO access_control (id, description, permission_create, permission_execute, permission_read, permission_update, permission_delete) VALUES (1, 'Controle Total', 'Y', 'Y', 'Y', 'Y', 'Y');
+INSERT INTO access_control (id, description, permission_create, permission_execute, permission_read, permission_update, permission_delete) VALUES (2, 'Apenas leitura',        'N', 'N', 'Y', 'N', 'N');
+INSERT INTO access_control (id, description, permission_create, permission_execute, permission_read, permission_update, permission_delete) VALUES (3, 'Leitura e execução',     'N', 'Y', 'Y', 'N', 'N');
+
+CREATE TABLE users(
+        id BIGINT AUTO_INCREMENT,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        mother_name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        phone VARCHAR(16) UNIQUE NOT NULL,
+        cpf VARCHAR(14) UNIQUE NOT NULL,
+        cep VARCHAR(9) NOT NULL,
+        address VARCHAR(255) NOT NULL,
+        birth DATE NOT NULL,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME,
+        deleted_at DATETIME,
+        PRIMARY KEY (id)
+    );
+
+CREATE TABLE modules(
+    id BIGINT AUTO_INCREMENT,
+    name varchar(50),
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO modules (id, name) VALUES (1, "profile");
+INSERT INTO modules (id, name) VALUES (2, "users");
+INSERT INTO modules (id, name) VALUES (3, "log");
+INSERT INTO modules (id, name) VALUES (4, "reports");
+INSERT INTO modules (id, name) VALUES (5, "permissions");
+
+CREATE TABLE access_control_modules_users(
+    id_access_control BIGINT,
+    user_id BIGINT,
+    module_id BIGINT,
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME,
+    FOREIGN KEY (id_access_control) REFERENCES access_control(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (module_id) REFERENCES modules(id)
+);
+
+CREATE TABLE auth(
+    id BIGINT AUTO_INCREMENT,
+    user_id bigint UNIQUE NOT NULL,
+    basic_token varchar(255) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME,
+    deleted_at DATETIME,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE log(
+    id BIGINT AUTO_INCREMENT,
+    user_id bigint NOT NULL,
+    changed_entity_id bigint NOT NULL,
+    type_log varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME,
+    deleted_at DATETIME,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE asks_2fa(
+        id BIGINT AUTO_INCREMENT,
+        ask_1 varchar(255) DEFAULT "Qual o nome da sua mãe",
+        ask_2 varchar(255) DEFAULT "Qual a data do seu nascimento",
+        ask_3 varchar(255) DEFAULT "Qual o CEP do seu endereço",
+        PRIMARY KEY (id)
+    );
+
+INSERT INTO asks_2fa (id, ask_1, ask_2, ask_3) VALUES (1, "Qual o nome da sua mãe", "Qual a data do seu nascimento", "Qual o CEP do seu endereço");
