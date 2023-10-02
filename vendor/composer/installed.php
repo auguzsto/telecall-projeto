c8822feffd05ca657c8d30187d4e3cd988d34aca
Matheus Augusto <auguzsto@gmail.com>
2023-10-02 14:24:16 -0300

    fix: add vendor in git

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'debian/telecall-projeto',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '67a8b1d62998736b78cb53aefc8b8bb8c35b1f43',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'debian/telecall-projeto' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '67a8b1d62998736b78cb53aefc8b8bb8c35b1f43',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
