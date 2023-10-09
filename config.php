c74dd197313357b4b573335b0d0d8046b44ae176
Matheus Augusto <auguzsto@gmail.com>
2023-10-09 16:35:09 -0300

    fix: back ip default in config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
     global $config;
     
-    $config['host'] = "mariadb";    // Change the host to 'mariadb' if you are using docker compose.
+    $config['host'] = "127.0.0.1";    // Change the host to 'mariadb' if you are using docker compose.
     $config['port'] = "3306";       // Make sure there is no service on port 3306 if you use docker compose.
     $config['user'] = "root";
     $config['password'] = "password";
